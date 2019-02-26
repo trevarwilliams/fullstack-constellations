e08c5c9ac1e653286b46bbfbe3e33c2dd0dfbc03
Trevar Williams <williams.trevar@gmail.com>
2019-02-26 14:45:44 -0800

    Formatting

--- api/models/users.js
+++ api/models/users.js
@@ -2,18 +2,20 @@
 
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   fullname: { type: String, default: "" },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   found: [
     {
       foundDate: { type: Date, default: Date.now },
-      constellation: { type: mongoose.Schema.Types.ObjectId, ref: "Constellation" }
+      constellation: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Constellation"
+      }
     }
   ]
 });
 
 const User = mongoose.model("User", userSchema);
-
 module.exports = { User };
