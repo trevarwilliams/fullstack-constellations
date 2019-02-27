066e2e7e7e6c395e69e58d8ab730b079824ba9dd
Trevar Williams <williams.trevar@gmail.com>
2019-02-26 18:01:08 -0800

    Bcrypt password hashing & stopped password storing to database

--- api/models/users.js
+++ api/models/users.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   fullname: { type: String, default: "" },
@@ -16,5 +17,24 @@ const userSchema = new mongoose.Schema({
     }
   ]
 });
+
+userSchema.set("timestamps", true);
+
+userSchema.set("toObject", {
+  transform: function(doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+    delete ret.password;
+  }
+});
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 10);
+};
 
 module.exports = mongoose.model("User", userSchema);
