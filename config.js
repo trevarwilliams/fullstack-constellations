335d542113b88c1b0cb12704859693e6645bbddb
Trevar Williams <williams.trevar@gmail.com>
2019-03-05 13:32:05 -0800

    Updated config URIs

---
+++ config.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+  PORT: process.env.PORT || 8080,
+  MONGODB_URI:
+    process.env.MONGODB_URI || "mongodb://localhost/constellation-companion-db",
+  TEST_MONGODB_URI:
+    process.env.TEST_MONGODB_URI ||
+    "mongodb://localhost/constellation-companion-db-test",
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "7d"
+};
